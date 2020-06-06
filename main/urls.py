0a6f4d88625c2a1e32153e5d22bed82d9f5364c8
park sang hyeop <mon2210@naver.com>
2020-06-06 14:38:02 +0900

    draw_line

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path('', views.analysis_view, name="analysis"),
+    path('test/', views.test, name="test"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-
+from main.models import Post
+import analysis
 
 def analysis_view(request):
 
@@ -10,3 +11,23 @@ def analysis_view(request):
     }
     # json_dumps_params = {"ensure_ascii":True} //문자 아스키코드로 바꿔서 전달할때 추가하기
     return JsonResponse(data)
+
+
+def test(request):
+
+    if request.method == "POST":
+        
+        post = Post()
+        post.name = request.FILES["image"].name 
+        post.image = request.FILES["image"]
+        post.save()
+        
+        analysis.draw_line(post.pk)
+
+        data = {
+            "message" : "message",
+            "image" : post.image.url 
+        }
+        return JsonResponse(data)
+    else:
+        return render(request, "test.html")
